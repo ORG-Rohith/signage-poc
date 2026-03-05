2fa70fcd11611a9fdfb1c152e9cfc1fde5c61f1d
Rohith <rohith@Rohiths-MacBook-Air.local>
2026-03-05 12:06:44 +0530

    change db string from .env

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,18 +1,22 @@
-import { Injectable,OnModuleInit } from "@nestjs/common";
+import 'dotenv/config';
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "generated/prisma/client";
 import { Pool } from 'pg';
 
-const pool = new Pool({ connectionString: "postgresql://postgres:1234@localhost:5434/poc" });
+const connectionString = process.env.DATABASE_URL ||
+  `postgresql://${process.env.DB_USER || 'postgres'}:${process.env.DB_PASSWORD || '1234'}@${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || '5434'}/${process.env.DB_NAME || 'poc'}`;
+
+const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit{
-    constructor() {
-    super({ adapter });  // ← pass adapter!
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({ adapter });
   }
-    
-    async onModuleInit() {
-        await this.$connect();
-    }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
